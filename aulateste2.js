2de78f5bc09e1cacc2046347b661d675d7d9bbe7
unknown <Administrador@GI-COL-TADS-10.integrado.int>
2025-04-08 20:27:19 -0300

    numeros pares

---
+++ aulateste2.js
@@ -0,0 +1,14 @@
+for (let x = 0; x <= 10; x++) {
+    // Código do loop aqui
+     process.stdout.write(x + ",");
+ }
+
+let contadora =0;
+for(let ii = 12;ii<237; ii++){
+    if(ii % 2 ==0){
+        contadora++;
+
+        contador = contadora +1;
+    }
+}
+console.log("existem "+contadora+" numerospares entre 121 e 237")
